cce3417cdda3da65e6006fc830612f0b73bbb26d
Mahesh Vanam <maheshvanam09@gmail.com>
2020-07-18 11:03:48 +0530

    Add. added README.md file

---
+++ PeginationSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PeginationSample
+//
+//  Created by admin on 17/07/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
